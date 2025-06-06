7972fb299f32cf4edc83cfd80a1af81cfb43529a
Gabriel Formario <49768089+gaformario@users.noreply.github.com>
2025-06-06 16:26:12 -0300

    feat: add application logs with Slf4j

--- src/main/java/com/gaformario/desafio_backend_itau/business/services/EstatiscaService.java
+++ src/main/java/com/gaformario/desafio_backend_itau/business/services/EstatiscaService.java
@@ -3,11 +3,13 @@ package com.gaformario.desafio_backend_itau.business.services;
 import com.gaformario.desafio_backend_itau.controller.dto.EstatisticasResponseDTO;
 import com.gaformario.desafio_backend_itau.controller.dto.TransacaoRequestDTO;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.DoubleSummaryStatistics;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class EstatiscaService {
@@ -15,15 +17,22 @@ public class EstatiscaService {
     public final TransacaoService transacaoService;
 
     public EstatisticasResponseDTO estatiscasTransacoes(Integer intervalo) {
+        log.info("Buscando estatísticas de transações para os últimos {} segundos", intervalo);
+
         List<TransacaoRequestDTO> transacoes = transacaoService.buscaTransacoes(intervalo);
 
-        DoubleSummaryStatistics stats = transacoes.stream()
-                        .mapToDouble(TransacaoRequestDTO::valor).summaryStatistics();
-
         if (transacoes.isEmpty()) {
+            log.warn("Nenhuma transação encontrada no intervalo especificado.");
             return new EstatisticasResponseDTO(0L,0.0,0.0,0.0,0.0);
         }
 
+        DoubleSummaryStatistics stats = transacoes.stream()
+                .mapToDouble(TransacaoRequestDTO::valor).summaryStatistics();
+
+        log.info("Estatísticas calculadas: count={}, sum={}, avg={}, min={}, max={}",
+                stats.getCount(), stats.getSum(), stats.getAverage(), stats.getMin(), stats.getMax());
+
+        log.info("Estatisticas retornadas com sucesso");
         return new EstatisticasResponseDTO(
                 stats.getCount(),
                 stats.getSum(),
--- src/main/java/com/gaformario/desafio_backend_itau/business/services/TransacaoService.java
+++ src/main/java/com/gaformario/desafio_backend_itau/business/services/TransacaoService.java
@@ -3,12 +3,14 @@ package com.gaformario.desafio_backend_itau.business.services;
 import com.gaformario.desafio_backend_itau.controller.dto.TransacaoRequestDTO;
 import com.gaformario.desafio_backend_itau.infrastrucutre.exceptions.UnprocessableEntity;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class TransacaoService {
@@ -16,7 +18,10 @@ public class TransacaoService {
     private final List<TransacaoRequestDTO> listaTransacoes = new ArrayList<>();
 
     public void adicionaTransacao(TransacaoRequestDTO dto) {
+        log.info("Processo para adicionar nova transação: {}", dto);
+
         if(dto.dataHora().isAfter(OffsetDateTime.now()) || dto.valor() < 0) {
+            log.error("Transação inválida: dataHora={}, valor={}", dto.dataHora(), dto.valor());
             throw new UnprocessableEntity("Campos inválidos na criação da transação");
         }
 
@@ -24,12 +29,17 @@ public class TransacaoService {
     }
 
     public void limpaTransacoes() {
+        log.info("Iniciando processo de limpeza das transações");
         listaTransacoes.clear();
+        log.info("Transações deletadas com sucesso");
     }
 
     public List<TransacaoRequestDTO> buscaTransacoes(Integer intervalo) {
+        log.info("Busca de transações com o seguinte intervalo de tempo {}", intervalo);
+
         OffsetDateTime dataHoraIntervalo = OffsetDateTime.now().minusSeconds(intervalo);
 
+        log.info("Transações retornadas com sucesso");
         return listaTransacoes.stream().
                 filter( transacoes -> transacoes.dataHora()
                         .isAfter(dataHoraIntervalo)).toList();
