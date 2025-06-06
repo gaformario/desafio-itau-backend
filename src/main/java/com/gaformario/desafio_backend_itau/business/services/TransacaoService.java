6dbe625ca3003ab065e2fe2ffc388fad034d9ee5
Gabriel Formario <49768089+gaformario@users.noreply.github.com>
2025-06-06 00:05:58 -0300

    feat: add DELETE transaction endpoint and add detail API documentation

--- src/main/java/com/gaformario/desafio_backend_itau/business/services/TransacaoService.java
+++ src/main/java/com/gaformario/desafio_backend_itau/business/services/TransacaoService.java
@@ -22,4 +22,8 @@ public class TransacaoService {
 
         listaTransacoes.add(dto);
     }
+
+    public void limpaTransacoes() {
+        listaTransacoes.clear();
+    }
 }
--- src/main/java/com/gaformario/desafio_backend_itau/controller/TransacaoController.java
+++ src/main/java/com/gaformario/desafio_backend_itau/controller/TransacaoController.java
@@ -2,14 +2,15 @@ package com.gaformario.desafio_backend_itau.controller;
 
 import com.gaformario.desafio_backend_itau.business.services.TransacaoService;
 import com.gaformario.desafio_backend_itau.controller.dto.TransacaoRequestDTO;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RequiredArgsConstructor
@@ -21,8 +22,28 @@ public class TransacaoController {
 
 
     @PostMapping
-    public ResponseEntity<Void> adicionarTransacao(@Validated @RequestBody TransacaoRequestDTO dto) {
+    @Operation(summary = "Recebe uma nova transação")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Transação gravada com sucesso"),
+            @ApiResponse(responseCode = "422", description = "Campos não atendem os requisitos da transação"),
+            @ApiResponse(responseCode = "400", description = "Erro de requisição (JSON inválido)"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    public ResponseEntity<Void> adicionarTransacao(
+            @Parameter(description = "Dados da transação a ser adicionada", required = true)
+            @Valid @RequestBody TransacaoRequestDTO dto) {
         transacaoService.adicionaTransacao(dto);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping
+    @Operation(summary = "Limpa e deleta todas as transações")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Transações deletadas com sucesso"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    public ResponseEntity<Void> deletarTransacoes() {
+        transacaoService.limpaTransacoes();
+        return ResponseEntity.ok().build();
     }
 }
